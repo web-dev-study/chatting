d3712766dc06ba465f672bbfcde4c6208be76a9d
Noah <23338687+enjay27@users.noreply.github.com>
2023-06-20 23:48:01 +0900

    build: add mongoose module with local connection environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+        user: config.get<string>('MONGODB_USERNAME'),
+        pass: config.get<string>('MONGODB_PASSWORD'),
+        dbName: config.get<string>('MONGODB_DB_NAME'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
