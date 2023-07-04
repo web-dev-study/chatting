0a2640e7fe9b7621a276e47945cd58074a58aabc
Noah <23338687+enjay27@users.noreply.github.com>
2023-07-05 00:42:06 +0900

    add mongodb local environment with docker compose file (#8)
    * fix: change root dir of test sets
    
    * fix: use environment variable NODE_ENV
    
    * fix: import ConfigService in app.controller test
    
    * feat: add MIT license
    
    * feat: add mongodb docker compose

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,17 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, ConfigService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    configService = app.get<ConfigService>(ConfigService);
   });
 
   describe('root', () => {
